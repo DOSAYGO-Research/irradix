187013ff1fb54c4713ef76fef7d9c8c29f931014
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-12-05 07:11:11 +0000

    "OK"

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-import {irradix} from './index.js';
+import {wholify,irradix} from './index.js';
+
+const RADIX = Math.PI;
 
 console.log(irradix);
 
 for( let i = 0; i < 1024; i ++ ) {
-  console.log(`${i} in base pi`, irradix(i, Math.PI));
+  const [A,B] = [Math.floor(RADIX), Math.ceil(RADIX)];
+  const baseA = i.toString(A);
+  const baseB = i.toString(B);
+  const baseI = irradix(i, Math.PI);
+  const back = wholify(baseI, Math.PI);
+  console.log(`${i} in base pi. (${baseA}_${A}, ${baseB}_${B})`, baseI, back, back === i);
 }
