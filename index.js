7ad9ed5ce6a79eec0041c72f39a7e16d05e144f1
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-12-08 10:48:19 +0000

    "OK"

--- index.js
+++ index.js
@@ -1,112 +1,61 @@
-const DEBUG = false;
-
-export function irradix(num, radic = Math.PI, subtractOrderOnly = false, wholeDivisorOnly = false) {
-  const rep = []; 
-
-  const epsilon = radic**-(Math.log(num)/Math.log(radic));
-
-  const rem = num % radic;
-  const unit = Math.ceil(rem) - 1;
-
-  rep.unshift(unit);
-
-  num -= rem;
-
-  while( Math.abs(num) > epsilon ) {
-    num /= radic; 
-    const newRem = num % radic;
-    const newUnit = Math.round(newRem);
-
-    rep.unshift(unit);
-
-    num -= newRem;
+export function irradix(num, radic = Math.PI) {
+  if ( ! Number.isInteger(num) ) {
+    throw new TypeError(`Sorry cannot convert non-integer numbers.`)
   }
 
-  return rep.join('');
+  const S = Math.sign(num);
+
+  if ( S == -1 ) {
+    num *= S;
+  }
+
+  const w = [num, 0];
+  const r = []; 
+  let lastW = Array.from(w);
+
+  const quanta = radic**-(Math.log(num)/Math.log(radic));
+  const epsilon = Number.EPSILON;
+
+  while( w[0] > epsilon ) {
+    w[1] = w[0] % radic;
+    w[0] = w[0] - w[1];
+    w[0] = w[0] / radic;
+
+    if ( w[0] >= lastW[0] ) {
+      break;
+    }
+
+    const unit = Math.floor(w[1]).toString(36);
+    r.unshift(unit);
+    
+    lastW = Array.from(w);
+  }
+
+  if ( S == -1 ) {
+    r.unshift('-');
+  }
+
+  if ( radic > 36 ) {
+    return r.join(',');
+  } else {
+    return r.join('');
+  }
 }
 
-function findOrder(residue, num, radic) {
-  const low = Math.max(radic,Math.floor(num - radic));
-  const high = Math.ceil(num + radic);
+export function derradix(rep, radic = Math.PI) {
+  const S = rep[0] === '-' ? -1 : 1;
 
-  const Res = [ ];
-
-  let index, j = 0, minDif = Infinity, minIndex = undefined;
-
-  for( let i = low; i <= high; i++ ) {
-    const resi = i % radic; 
-    Res.push(resi);
-    if ( index === undefined ) {
-      if ( resi === residue ) {
-        index = j;
-      } else if ( Math.abs(resi-residue) < minDif ) {
-        minDif = Math.abs(resi-residue);
-        minIndex = j;
-      }
-    }
-    j++;
+  if ( S === -1 ) {
+    rep = rep.slice(1);
   }
 
-  if ( index === undefined ) {
-    DEBUG && console.log({minDif, minIndex});
-    index = minIndex;
-    DEBUG && console.log({Res,low,high,num,radic,residue,index});
-  }
-
-  const Order = [];
-  let order, lastRes = -Infinity, run = 0, maxRun = -Infinity;
-  let first0;
-
-  for( let i = 0; i < Res.length; i++ ) {
-    const res = Res[i];
-    if ( res === 0 ) {
-      first0 = i;
-      order = -1;
-    }
-    if ( res > lastRes ) {
-      run += 1;
-      if ( order !== undefined ) {
-        order++;
-      }
-    } else {
-      if ( run > maxRun ) {
-        maxRun = run;
-      }
-      if ( order === undefined ) {
-        first0 = i;
-      }
-      run = 0;
-      order = 0;
-    }
-    //console.log({order, num, maxRun, res, lastRes, first0, i})
-    Order.push(order); 
-    lastRes = res;
-  }
-
-  for( let i = 0; i < first0; i++ ) {
-    Order[i] = maxRun - (first0 - i - 1);
-  }
-
-  const unit = Order[index];
-  if ( unit > (Math.floor(radic)-1) ) {
-    DEBUG && console.log({minDif, minIndex, unit});
-    DEBUG && console.log({Order,Res,low,high,num,radic,residue,index});
-  } 
-  return unit;
-}
-
-export function wholify(rep, radic = Math.PI) {
-  rep = rep.split('').reverse().map(u => parseInt(u));
-  let num = 1;
+  rep = rep.split(radic > 36 ? ',' : '').map(u => parseInt(u, 36));
+  let num = 0;
   for(let u of rep) {
-    u = u + 1;
-    // count up to the uth whole number above num
-    while(u--) {
-      num = Math.floor(num + 1);
-    }
     num *= radic;
+    num = Math.ceil(num + u);
   }
 
-  return num;
+  return S*num;
 }
 
--- test.js
+++ test.js
@@ -1,14 +1,25 @@
-import {wholify,irradix} from './index.js';
+import {derradix,irradix} from './index.js';
 
-const RADIX = Math.PI;
+const VALS = {
+  PI: Math.PI,
+  SQRT2: Math.SQRT2,
+  PHI: 0.5+Math.sqrt(5)/2,
+  DELTA: 1+Math.SQRT2,
+  E: Math.E,
+  G: Math.E**Math.PI,
+  G20: Math.E**Math.PI-Math.PI
+};
 
-console.log(irradix);
+const RADIX = VALS[process.argv[2] || 'PI'] || parseFloat(process.argv[2]);
+//(1 + Math.sqrt(5))/2;
 
-for( let i = 0; i < 1024; i ++ ) {
-  const [A,B] = [Math.floor(RADIX), Math.ceil(RADIX)];
+for( let i = -1000; i <= 1000; i ++ ) {
+  const [A,B] = [Math.max(2,Math.min(36,Math.floor(RADIX))), Math.min(Math.max(2,Math.ceil(RADIX)),36)];
   const baseA = i.toString(A);
   const baseB = i.toString(B);
-  const baseI = irradix(i, Math.PI);
-  const back = wholify(baseI, Math.PI);
-  console.log(`${i} in base pi. (${baseA}_${A}, ${baseB}_${B})`, baseI, back, back === i);
+  const baseI = irradix(i, RADIX);
+  const back = derradix(baseI, RADIX);
+  console.log(
+    `${i} in base ${RADIX}: ${baseI} ${back} ${i === back} (${baseA}_${A}, ${baseB}_${B})`
+  );
 }
