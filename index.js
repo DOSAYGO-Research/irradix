43beb6dda30ca03fdce1d2a6a380cd4eb568adcb
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-12-18 16:31:51 +0000

    "tired"

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+export const VALS = {
+  PI: Math.PI,
+  SQRT2: Math.SQRT2,              /* no OEIS */
+  PHI: 0.5+Math.sqrt(5)/2,        /* golden mean, as binary is A336231 */
+  "2PHI": 2/(0.5+Math.sqrt(5)/2), /* 2-inverse of golden mean, no OEIS */
+  DELTA: 1+Math.SQRT2,            /* silver mean */
+  BRONZE: (3+Math.sqrt(13))/2,    /* bronze mean */
+  SQRT3: Math.sqrt(3),
+  SQRT3IPHI: Math.sqrt(3+1/(0.5+Math.sqrt(5)/2)),
+  E: Math.E,
+  G: Math.E**Math.PI,
+  G20: Math.E**Math.PI-Math.PI,
+  G20T: (Math.E**Math.PI-Math.PI)/10
+};
+
 export function irradix(num, radic = Math.PI) {
   if ( num === 0 ) {
     return "0";
@@ -77,3 +92,88 @@ export function derradix(rep, radic = Math.PI) {
   return S*num;
 }
 
+// encode positive javascript integers of any size (not floats, not bigints)
+// into a packed array with members of arbitrary bit length
+export function encode(nums, bits = 8) {
+  nums = nums.map(x => irradix(x, VALS.PHI)); // every number now has no '101' in it
+  console.log(nums);
+  nums = nums.map(x => x.endsWith('10') ? x + '0101' : x); // add extra 0
+  nums = nums.join('10');                     // now every number is separated by 1-0-1
+
+  console.log(nums);
+  nums = chunk(nums, bits);
+
+  nums = nums.map(s => parseInt(s,2));
+  
+  if ( bits > 6 ) {
+    return nums;
+  } else {
+    nums = nums.map(x => ALPHABET[bits][x]);
+  }
+}
+
+/* probably need to handle last chunk somehow */
+function chunk(str, size) {
+  const chunks = []; 
+  let nextChunk = '';
+
+  let i = 0;
+
+  while(i < str.length) {
+    nextChunk += str[i];
+    i++;
+    if ( nextChunk.length < size ) continue;
+
+    chunks.push(nextChunk);
+    nextChunk = '';
+  }
+
+  return chunks;
+}
+
+export function decode(chunks) {
+  chunks = chunks.map(n => n.toString(2)); 
+  chunks = chunks.join('');
+  let realChunks = [];
+  console.log(chunks);
+  while(chunks.length) {
+    if ( chunks.slice(0,3) == '101' ) {
+      chunks = chunks.slice(3);
+      if ( chunks.slice(0,3) == '101' ) {
+        chunks = chunks.slice(3);
+        realChunks.push('');
+      }
+    } else {
+      const next = chunks.indexOf('101');
+      if ( next == -1 ) {
+        realChunks.push(chunks);
+        break;
+      }
+      realChunks.push(chunks.slice(0,next));
+      chunks = chunks.slice(next);
+    }
+  }
+  console.log(realChunks);
+  chunks = Array.from(realChunks);
+  for ( let i = 0; i < chunks.length; i++ ) {
+    let chunk = chunks[i];
+    console.log(chunk);
+
+    if ( chunk == '' ) continue;
+
+    if ( i > 0 ) {
+      chunk = '1' + chunk; // add extra one lost in 101 split
+    }
+
+    if ( i < chunks.length-1 ) {
+      if ( chunks[i+1] == '' ) {
+        chunk = chunk.slice(0,-1); // remove extra zero added if ended with '10'
+      }
+    }
+
+    realChunks.push(chunk);
+  }
+  console.log(realChunks);
+  chunks = realChunks.map(c => parseInt(c,2));
+  return chunks;
+}
--- test.js
+++ test.js
@@ -1,61 +1,57 @@
-import {derradix,irradix} from './index.js';
+import {encode, decode, VALS,derradix,irradix} from './index.js';
 
-const VALS = {
-  PI: Math.PI,
-  SQRT2: Math.SQRT2,              /* no OEIS */
-  PHI: 0.5+Math.sqrt(5)/2,        /* golden mean, as binary is A336231 */
-  "2PHI": 2/(0.5+Math.sqrt(5)/2), /* 2-inverse of golden mean, no OEIS */
-  DELTA: 1+Math.SQRT2,            /* silver mean */
-  BRONZE: (3+Math.sqrt(13))/2,    /* bronze mean */
-  SQRT3: Math.sqrt(3),
-  SQRT3IPHI: Math.sqrt(3+1/(0.5+Math.sqrt(5)/2)),
-  E: Math.E,
-  G: Math.E**Math.PI,
-  G20: Math.E**Math.PI-Math.PI,
-  G20T: (Math.E**Math.PI-Math.PI)/10
-};
+testCodec();
 
-const RADIX = VALS[process.argv[2] || 'PI'] || parseFloat(process.argv[2]);
-
-const MARK = '+';
-const GAP = '-';
-const PATTERN_WIDTH = 64;
-let pattern = ['+'];
-let lastN = 0;
-let count = 1;
-
-for( let i = 0; i <= 20; i ++ ) {
-  const [A,B] = [Math.max(2,Math.min(36,Math.floor(RADIX))), Math.min(Math.max(2,Math.ceil(RADIX)),36)];
-  const baseA = i.toString(A);
-  const baseB = i.toString(B);
-  const baseI = irradix(i, RADIX);
-  const back = derradix(baseI, RADIX);
-  const N = parseInt(baseI,B);
-  for( let j = lastN; j < N; j++ ) {
-    if ( j === lastN ) {
-      pattern.push(MARK);
-    } else { 
-      pattern.push(GAP);
-    }
-    //console.log("j",j);
-    count++;
-    if ( (j+1)% PATTERN_WIDTH === 0 ) {
-      //console.log("Pattern length", pattern.length);
-      //console.log("Break");
-      pattern.push('\n');
-    }
-  }
-  //console.log("N",N);
-  lastN = N;
-  console.log(
-    `${i} in base ${RADIX}: ${baseI} ${baseI.length} (${parseInt(baseI,B)}) ${back} ${i === back} (${baseA}_${A}, ${baseB}_${B})`
-  );
+function testCodec() {
+  const X = [1,2,3,4,5,6,7,8,9,10];
+  const packed = encode(X, 8);
+  const unpacked = decode(packed);
+  console.log({X,packed,unpacked});
 }
 
-pattern.push('+')
+function testRadix() {
+  const RADIX = VALS[process.argv[2] || 'PI'] || parseFloat(process.argv[2]);
 
-count++;
+  const MARK = '+';
+  const GAP = '-';
+  const PATTERN_WIDTH = 64;
+  let pattern = ['+'];
+  let lastN = 0;
+  let count = 1;
 
-console.log('');
-console.log(pattern.join('') + '\n');
-console.log(count);
+  for( let i = 0; i <= 20; i ++ ) {
+    const [A,B] = [Math.max(2,Math.min(36,Math.floor(RADIX))), Math.min(Math.max(2,Math.ceil(RADIX)),36)];
+    const baseA = i.toString(A);
+    const baseB = i.toString(B);
+    const baseI = irradix(i, RADIX);
+    const back = derradix(baseI, RADIX);
+    const N = parseInt(baseI,B);
+    for( let j = lastN; j < N; j++ ) {
+      if ( j === lastN ) {
+        pattern.push(MARK);
+      } else { 
+        pattern.push(GAP);
+      }
+      //console.log("j",j);
+      count++;
+      if ( (j+1)% PATTERN_WIDTH === 0 ) {
+        //console.log("Pattern length", pattern.length);
+        //console.log("Break");
+        pattern.push('\n');
+      }
+    }
+    //console.log("N",N);
+    lastN = N;
+    console.log(
+      `${i} in base ${RADIX}: ${baseI} ${baseI.length} (${parseInt(baseI,B)}) ${back} ${i === back} (${baseA}_${A}, ${baseB}_${B})`
+    );
+  }
+
+  pattern.push('+')
+
+  count++;
+
+  console.log('');
+  console.log(pattern.join('') + '\n');
+  console.log(count);
+}
